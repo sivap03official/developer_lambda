16aa528f72a651298b428588a3f7c21f628bdbb9
Siva P <sivatena@gmail.com>
2026-02-12 17:46:53 +0530

    cors added

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 const extract = (event) => {
     try {
-      return JSON.stringify(event)
-    } catch(e) {
-      return { error: e?.message }
+        return JSON.stringify(event)
+    } catch (e) {
+        return JSON.stringify({ error: e?.message })
     }
 }
 
-export const handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: extract(event),
-  };
-  return response;
+const handler = async (event) => {
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: extract(cors(event)),
+    };
+    return response;
 };
+
+const filterHeaders = (event) => {
+    const allowHeaders = ["authorization", "content-type", "host"]
+
+    if (!event?.headers) {
+        return event
+    }
+
+    const filteredHeaders = {}
+    for (const [key, value] of Object.entries(event.headers)) {
+        if (allowHeaders.includes(key.toLowerCase())) {
+            filteredHeaders[key] = value
+        }
+    }
+    filteredHeaders['x-allowed-headers'] = Object.keys(filteredHeaders).join(',')
+    event.headers = filteredHeaders
+    return event
+}
+
+const cors = (event) => {
+    if (!event) {
+        return event
+    }
+    const context = event?.requestContext || {}
+    if (context?.http?.path) {
+        context.http.path = decodeURIComponent(context.http.path)
+    }
+    const method = context?.http?.method
+    console.log(`current request id ${context?.requestId}, path: ${context?.http?.path}, method: ${method}`)
+    if (!(method && ["OPTIONS", "GET", "POST", "PUT", "DELETE"].includes(method.toUpperCase()))) {
+        throw new Error(`Invalid HTTP method: ${method}`)
+    }
+    event = filterHeaders(event)
+    return event
+}
+
+module.exports = { handler };

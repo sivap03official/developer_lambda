bff55d2912a24b6d363d8bd5455d9d44498629b8
Siva P <sivatena@gmail.com>
2026-02-13 11:59:26 +0530

    schemaGen.js

--- src/schemaGen.js
+++ src/schemaGen.js
@@ -1,19 +1,20 @@
 const schemagenerator = require("fluent-json-schema")
+const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const signUp = schemagenerator.object()
     .prop("name", schemagenerator.string().minLength(3).maxLength(50).required())
-    .prop("email", schemagenerator.string().format("email").required())
+    .prop("email", schemagenerator.string().pattern(pattern).required())
     .prop("password", schemagenerator.string().minLength(8).maxLength(16).pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]").required())
     .prop("mobile", schemagenerator.string().pattern("^[0-9]{10}$")).valueOf()
 
 const logIn = schemagenerator.object()
-    .prop("email", schemagenerator.string().format("email").required())
+    .prop("email", schemagenerator.string().pattern(pattern).required())
     .prop("password", schemagenerator.string().minLength(8).maxLength(16).pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]").required())
     .valueOf()
 
 const address = schemagenerator.object()
     .prop("name", schemagenerator.string().minLength(3).maxLength(50).required())
-    .prop("email", schemagenerator.string().format("email").required())
+    .prop("email", schemagenerator.string().pattern(pattern).required())
     .prop("mobile", schemagenerator.string().pattern("^[0-9]{10}$")).required()
     .prop("addressLine1", schemagenerator.string().required())
     .prop("addressLine2", schemagenerator.string())
